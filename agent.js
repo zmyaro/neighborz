a50f15d46732658220d29ed49d6ffcc1acb0f386
Zachary Yaro <zmyaro@zmyaro.com>
2016-04-11 11:17:31 -0400

    Initial commit

---
+++ agent.js
@@ -0,0 +1,144 @@
+'use strict';
+
+function Agent(x, y, type) {
+	this.elem = document.createElement('div');
+	this.elem.agent = this;
+	this.elem.className = 'agent';
+	this._x = x || 0;
+	this._y = y || 0;
+	this.moveTo(this._x, this._y);
+	this.type = type;
+	this.size = grid.cellSize;
+	document.body.appendChild(this.elem);
+}
+
+Agent.TYPE_COLORS = [
+	'orange',
+	'blue',
+	'magenta',
+	'yellow'
+];
+Agent.HAPPY_COLOR = 'lime';
+Agent.UNHAPPY_COLOR = 'red';
+
+Agent.prototype = {
+	destroy: function () {
+		document.body.removeChild(this.elem);
+	},
+	
+	get type() {
+		return this._type;
+	},
+	get x() {
+		return this._x;
+	},
+	get y() {
+		return this._y;
+	},
+	set size(val) {
+		this.elem.style.width = val + 'px';
+		this.elem.style.height = val + 'px';
+	},
+	set type(val) {
+		this.elem.style.backgroundColor = Agent.TYPE_COLORS[val];
+		this._type = val;
+	},
+	set x(val) {
+		this.elem.style.transform = 'translate(' + (val * grid.cellSize) + 'px,' + (this._y * grid.cellSize) + 'px) scale(0.9)';
+		this._x = val;
+	},
+	set y(val) {
+		this.elem.style.transform = 'translate(' + (this._x * grid.cellSize) + 'px,' + (val * grid.cellSize) + 'px) scale(0.9)';
+		this._y = val;
+	},
+	
+	checkHappiness: function () {
+		if (this.checkHappinessAt(this.x, this.y)) {
+			this.elem.style.borderColor = Agent.HAPPY_COLOR;
+			return true;
+		} else {
+			this.elem.style.borderColor = Agent.UNHAPPY_COLOR;
+			return false;
+		}
+	},
+	checkHappinessAt: function (x, y) {
+		var neighborSpaces = 0,
+			similarNeighbors = 0;
+		for (var r = Math.max(0, y - 1); r <= Math.min(grid.height - 1, y + 1); r++) {
+			for (var c = Math.max(0, x - 1); c <= Math.min(grid.width - 1, x + 1); c++) {
+				if (typeof grid[r][c] !== 'undefined' && !(r === y && c === x)) {
+					neighborSpaces++;
+					if (grid[r][c].type === this.type) {
+						similarNeighbors++;
+					}
+				}
+			}
+		}
+		if (similarNeighbors / neighborSpaces >= happinessPercentage) {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	
+	getFreeSquares: function () {
+		var freeSquares = [];
+		for (var r = 0; r < grid.height; r++) {
+			for (var c = 0; c < grid.width; c++) {
+				if (typeof grid[r][c] === 'undefined') {
+					freeSquares.push({
+						x: c,
+						y: r
+					});
+				}
+			}
+		}
+		return freeSquares;
+	},
+	
+	getOptimalSquare: function (squares) {
+		var optimalSquare,
+			optimalSquareDist = Number.MAX_VALUE;
+		for (var i = 0; i < squares.length; i++) {
+			if (this.checkHappinessAt(squares[i].x, squares[i].y)) {
+				var newDist = dist(this.x, this.y, squares[i].x, squares[i].y);
+				if (newDist < optimalSquareDist) {
+					optimalSquareDist = newDist;
+					optimalSquare = squares[i];
+				}
+			}
+		}
+		return optimalSquare;
+	},
+	
+	move: function () {
+		// Do not move if already happy.
+		if (this.checkHappiness()) {
+			return;
+		}
+		// Look for a place to move.
+		var freeSquares = this.getFreeSquares(),
+			optimalSquare = this.getOptimalSquare(freeSquares);
+		// Move if one is found.
+		if (optimalSquare) {
+			this.moveTo(optimalSquare.x, optimalSquare.y);
+			return;
+		}
+		var i = Math.floor(Math.random() * freeSquares.length);
+		this.moveTo(freeSquares[i].x, freeSquares[i].y);
+	},
+	
+	moveTo: function (x, y) {
+		if (typeof grid[y][x] !== 'undefined' && grid[y][x] !== this) {
+			console.error('Tried to move to filled space.');
+		}
+		grid[this._y][this._x] = undefined;
+		grid[y][x] = this;
+		if (x >= 0 && x < grid.width) {
+			this.x = x;
+		}
+		if (y >= 0 && y < grid.height) {
+			this.y = y;
+		}
+	}
+};
---
+++ grid.js
@@ -0,0 +1,142 @@
+'use strict';
+
+var agentTypes = 2,
+	happinessPercentage = 0.4,
+	playing = false,
+	timer,
+	grid = [];
+
+grid.width = 10;
+grid.height = 10;
+grid.cellSize = 64;
+for (var r = 0; r < grid.height; r++) {
+	grid[r] = new Array(grid.width);
+}
+
+window.onload = function () {
+	document.getElementById('aboutBtn').onclick = function (e) {
+		alert('NeighborZ explores prejudice and segregation based on tolerence of those we perceive as different from us.  As tolerance of neighbors who are different decreases, AIs fairly consistently end up clustering into segregated neighborhoods, sometimes even when they are 70% or 80% tolerant.');
+	}
+	document.getElementById('agentTypesInput').oninput = function (e) {
+		agentTypes = parseInt(e.target.value);
+	};
+	document.getElementById('toleranceInput').oninput = function (e) {
+		happinessPercentage = 1 - parseFloat(e.target.value);
+	};
+	document.getElementById('restartBtn').onclick = function () {
+		initAgents();
+	};
+	document.getElementById('checkStatusBtn').onclick = function () {
+		checkStatus();
+	};
+	document.getElementById('runBtn').onclick = function () {
+		runStep();
+	};
+	document.getElementById('playPauseBtn').onclick = function () {
+		playing = !playing;
+		if (playing) {
+			runStep();
+		} else {
+			clearTimeout(timer);
+		}
+	};
+	
+	grid.elem = document.getElementById('gridCanvas');
+	resizeGrid();
+	initAgents();
+}
+
+function resizeGrid() {
+	grid.elem.width = grid.cellSize * grid.width + 1;
+	grid.elem.height = grid.cellSize * grid.height + 1;
+	var ctx = grid.elem.getContext('2d');
+	ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+	for (var r = 0; r < grid.height; r++) {
+		for (var c = 0; c < grid.width; c++) {
+			ctx.strokeRect(
+				r * grid.cellSize,
+				c * grid.cellSize,
+				grid.cellSize,
+				grid.cellSize);
+		}
+		if (typeof grid[r][c] !== 'undefined') {
+			grid[r][c].size = grid.cellSize;
+		}
+	}
+}
+
+function initAgents() {
+	
+	// Delete the old agents.
+	for (var r = 0; r < grid.height; r++) {
+		for (var c = 0; c < grid.width; c++) {
+			if (typeof grid[r][c] !== 'undefined') {
+				grid[r][c].destroy();
+			}
+		}
+	}
+	
+	// Create a new agents array and populate all but the four corners.
+	for (var r = 0; r < grid.height; r++) {
+		grid[r] = new Array(grid.width);
+		for (var c = 0; c < grid.width; c++) {
+			if ((r === 0 && c === 0) ||
+					(r === grid.height - 1 && c === 0) ||
+					(r === 0 && c === grid.width - 1) ||
+					(r === grid.height - 1 && c === grid.width - 1)) {
+				continue;
+			}
+			var type = (r * grid.width + c) % agentTypes;
+			grid[r][c] = new Agent(c, r, type);
+		}
+	}
+	
+	// Delete twenty more at random.
+	for (var i = 0; i < Math.floor(0.3125 * grid.width * grid.height); i++) {
+		var r = Math.floor(Math.random() * grid.height),
+			c = Math.floor(Math.random() * grid.width);
+		if (typeof grid[r][c] !== 'undefined') {
+			grid[r][c].destroy();
+			grid[r][c] = undefined;
+		} else {
+			i--;
+		}
+	}
+	// Add five back in at random.
+	for (var i = 0; i < Math.floor(0.078125 * grid.width * grid.height); i++) {
+		var r = Math.floor(Math.random() * grid.height),
+			c = Math.floor(Math.random() * grid.width);
+		if (typeof grid[r][c] === 'undefined') {
+			grid[r][c] = new Agent(c, r, Math.floor(Math.random() * agentTypes));
+		} else {
+			i--;
+		}
+	}
+}
+
+function checkStatus() {
+	for (var r = 0; r < grid.height; r++) {
+		for (var c = 0; c < grid.width; c++) {
+			if (typeof grid[r][c] !== 'undefined') {
+				grid[r][c].checkHappiness();
+			}
+		}
+	}
+}
+function runStep() {
+	for (var r = 0; r < grid.height; r++) {
+		for (var c = 0; c < grid.width; c++) {
+			if (typeof grid[r][c] !== 'undefined') {
+				grid[r][c].move();
+			}
+		}
+	}
+	checkStatus();
+	if (playing) {
+		timer = console.log(setTimeout(runStep, 500));
+	}
+}
+
+function dist(x1, y1, x2, y2) {
+	return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+}
